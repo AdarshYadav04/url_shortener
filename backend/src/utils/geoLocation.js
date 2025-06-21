2a96c5d763a270d753e864bf06c1e1d7b4092a73
Adarsh Yadav <adarshyadav0404@gmail.com>
2025-06-21 10:03:00 +0530

    update in utils

--- backend/src/utils/geoLocation.js
+++ backend/src/utils/geoLocation.js
@@ -1,14 +1,5 @@
-import axios from "axios";
+
 export const getLocationFromIP = async (ip) => {
   
-  try {
-    const res = await axios.get(`https://ipapi.co/${ip}/country_name/`);
-    console.log(res)
-    
-    return  res.data || 'Unknown';
-    
-  } catch (err) {
-    console.error('IP lookup failed:', err.message);
-    return 'Unknown';
-  }
+     return `MockedLocation for ${ip}`;
 };
