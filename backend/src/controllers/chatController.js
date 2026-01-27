db08bcc6cda169ede1915bc861413e93d61e5ea0
Adarsh Yadav <adarshyadav0404@gmail.com>
2026-01-27 21:16:45 +0530

    bugs fixed

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -4,7 +4,7 @@ const PYTHON_API_URL = process.env.PYTHON_API_URL;
 export const chatController = async (req, res) => {
     const { question } = req.body;
     try {
-        const response = await axios.post(`${PYTHON_API_URL}/chat`, { question });
+        const response = await axios.post(`${PYTHON_API_URL}/chat`, { question },{timeout: 60000});
         return res.status(200).json(response.data);
     } catch (error) {
         console.error('Error in chatController:', error);
