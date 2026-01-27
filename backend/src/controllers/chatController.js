82fa6a44b39dea1fa20712e08a71aa66a3bbfc55
Adarsh Yadav <adarshyadav0404@gmail.com>
2026-01-27 20:32:07 +0530

    chatbot bugs fixed

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -4,10 +4,10 @@ const PYTHON_API_URL = process.env.PYTHON_API_URL;
 export const chatController = async (req, res) => {
     const { question } = req.body;
     try {
-        const response = await axios.post(PYTHON_API_URL, { question });
-        res.status(200).json(response.data);
+        const response = await axios.post(PYTHON_API_URL+"/chat", { question });
+        return res.status(200).json(response.data);
     } catch (error) {
         console.error('Error in chatController:', error);
-        res.status(500).json({ error: 'Error in chatController' });
+        return res.status(500).json({ error: 'Error in chatController' });
     }
 };
