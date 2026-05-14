f22fb87880f86b11ea162f30565f5f6f33bde1b6
Adarsh Yadav <adarshyadav0404@gmail.com>
2026-05-14 09:10:37 +0530

    chat bot bugs fixed

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,15 +1,31 @@
 import axios from "axios"
 const PYTHON_API_URL = process.env.PYTHON_API_URL;
+const PYTHON_API_KEY = process.env.PYTHON_API_KEY;
 
 export const chatController = async (req, res) => {
+  
     const { question } = req.body;
     try {
-        const response = await axios.post(`${PYTHON_API_URL}/chat`, { question },{
+        if (!PYTHON_API_URL) {
+          return res.status(500).json({ error: "PYTHON_API_URL is not set" });
+        }
+        if (!PYTHON_API_KEY) {
+          return res.status(500).json({ error: "PYTHON_API_KEY is not set" });
+        }
+
+        const response = await axios.post(`${PYTHON_API_URL}/chat`, {
+          query: question,
+          top_k: req.body?.top_k ?? 1,
+          temperature: req.body?.temperature ?? 2,
+          include_sources: req.body?.include_sources ?? true,
+        },{
             headers: {
-              "Content-Type": "application/json"
-            },
-            timeout: 60000
+              "accept": "application/json",
+              "Content-Type": "application/json",
+              "X-API-Key": PYTHON_API_KEY
+            }
           });
+        
         return res.status(200).json(response.data);
     } catch (error) {
         console.error('Error in chatController:', error);
