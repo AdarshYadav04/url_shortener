666183cf823b6c7e69f37a071d50674714cbdf1d
Adarsh Yadav <adarshyadav0404@gmail.com>
2026-05-16 14:11:02 +0530

    fixed location bugs

--- backend/src/utils/geoLocation.js
+++ backend/src/utils/geoLocation.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export const getLocationFromIP = async (ip) => {
   
   try {
-    const res = await axios.get(`https://ipapi.co/${ip}/country_name/`);
+    const res = await axios.get(`https://ipapi.co/${ip}/json`);
     console.log(res)
     
     return  res.data || 'Unknown';
